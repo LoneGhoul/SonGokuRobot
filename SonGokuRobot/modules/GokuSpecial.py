4dc24d1d907cdd2ed7a698dbc19915ba1ae9dc81
Goku <81037822+ImmaGoku@users.noreply.github.com>
2021-03-25 10:21:09 +0530

    Create GokuSpecial.py

---
+++ SonGokuRobot/modules/GokuSpecial.py
@@ -0,0 +1,40 @@
+
+__help__ = """
+*Gif and Images:*
+ • `/whatanime`*:* to search source of anime reply to photo or gif. 
+ 
+ *Image Search:*
+  • `/whatanime`*:* to search source of anime reply to photo or gif. 
+  • `/reverse`*:* to search source of Image reply to photo. 
+  • `/getqr <reply to image>`*:* Read QR code
+  • `/makeqr <reply to text>`*:* Make QR code
+  
+ *Movies And Series:*
+  • `/imdb`*:* to search source of Movie And Series.
+  
+ *covid:*
+  • `/covid`*:* To get data of a covid.
+ *Extra Fun With Goku:*
+ • `/q`*:* quote a replied message. Like QuotLyBot
+ • `/zombies`*:* Search and remove Deleted Accounts from Group.  
+ • `/img <keyword>`*:* to search Image any. 
+ • `/tts <your text>`*:*  text to speech.
+ • `/phone <tp no>`*:* Track Phone no
+*Stylish your text!*
+ • `/weebify <text>`*:* weebify your text!
+ • `/bubble <text>`*:* bubble your text!
+ • `/fbubble <text>`*:* bubble-filled your text!
+ • `/square <text>`*:* square your text!
+ • `/fsquare <text>`*:* square-filled your text!
+ • `/blue <text>`*:* bluify your text!
+ • `/latin <text>`*:* latinify your text!
+ • `/lined <text>`*:* lined your text!
+ • `/plet <text> `*:* text get funny emojify.
+ 
+ *Truth And Dare*
+ • `/Truth`*:* for random truth.
+ • `/dare`*:* for random dare.
+"""
+
+
+__mod_name__ = "Goku Specials"
