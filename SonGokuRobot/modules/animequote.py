516f8f92106faab638baf4903c2a978f2738e231
Goku <81037822+ImmaGoku@users.noreply.github.com>
2021-03-27 01:04:51 +0530

    motivation owo

---
+++ SonGokuRobot/modules/animequote.py
@@ -0,0 +1,18 @@
+# All credits for this module goes to @uday_gondaliya ©.
+
+import html
+import random
+import SonGokuRobot.modules.animequote_string as animequote_string
+from SonGokuRobot import dispatcher
+from telegram import ParseMode, Update, Bot
+from SonGokuRobot.modules.disable import DisableAbleCommandHandler
+from telegram.ext import CallbackContext, run_async
+
+@run_async
+def aq(update: Update, context: CallbackContext):
+    args = context.args
+    update.effective_message.reply_text(random.choice(animequote_string.ANIMEQUOTE))
+
+AQ_HANDLER = DisableAbleCommandHandler("aq", aq)
+
+dispatcher.add_handler(AQ_HANDLER)
